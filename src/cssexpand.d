bbffa5e9bb1aa1a97e418fbc79e39f62637f36f4
Adam D. Ruppe <destructionator@gmail.com>
2014-04-23 09:09:19 -0400

    dollar sign macro sigil

--- src/cssexpand.d
+++ src/cssexpand.d
@@ -1,5 +1,6 @@
 import std.file;
 import std.stdio;
+import std.array;
 import arsd.html;
 
 void main(string[] args) {
@@ -13,6 +14,12 @@ void main(string[] args) {
 	foreach(arg; args[1 .. $]) {
 		source ~= readText(arg);
 	}
+
+	// Let $ be used instead of ¤ in the source
+	// while still allowing __DOLLAR__ to be used as the
+	// literally CSS thing
+	source = source.replace("$", "¤");
+	source = source.replace("__DOLLAR__", "$");
 
 	auto me = new CssMacroExpander();
 	std.stdio.write(me.expandAndDenest(source));
